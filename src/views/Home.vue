72ac93bed6a9ccf89e6a3b43459aa52b2e529018
Osman Erol <osman.erol@devbase.com.tr>
2021-12-04 15:16:36 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style lang="scss" scoped>
+  
+</style>
